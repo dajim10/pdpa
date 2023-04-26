9e68da72a57f638931d6b0b5bfa9b9aa0bfed6ee
dajim10 <dajim10@hotmail.com>
2023-04-26 13:06:29 +0700

    edit text in header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
   return (
     <>
       <Helmet>
-          <title>Cyber Security</title>
-          <meta name="description" content="การคุ้มครองข้อมูลส่วนบุคคล มหาวิทยาลัยราชมงคลศรีวิชัย Cyber Security"/>
-          <link rel="canonical" href="/" />
+        <title>Cyber Security</title>
+        <meta name="description" content="การคุ้มครองข้อมูลส่วนบุคคล มหาวิทยาลัยราชมงคลศรีวิชัย Cyber Security" />
+        <link rel="canonical" href="/" />
       </Helmet>
       <div className="container mt-3 mb-3 d-flex justify-content-center align-items-end">
         <img src={LogoRmutsv} alt="Logo Rmutsv" width="50" />
@@ -19,7 +19,7 @@ function Header() {
       </div>
       <div className="container text-center">
         <p>
-          Cyber Security And Personal Data Protection Act(PDPA), Rajamangala University of
+          Cyber Security And Personal Data Protection Act (PDPA), Rajamangala University of
           Technology Srivijaya
         </p>
       </div>
