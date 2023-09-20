c616139f00e3df2483014cf95c7c224e6ee6acc3
varutcha.c <varutcha.c@rmutsv.ac.th>
2023-09-20 20:03:32 +0700

    add signed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import PDPA_IMG from '../images/Acting1.png'
 import Cyber_img from '../images/Acting2.png'
 import Modal from 'react-bootstrap/Modal';
@@ -7,6 +7,7 @@ import pdpa_2562 from '../pdf/announce_pdpa_rmutsv.pdf'
 import cyber15062023 from '../pdf/cyberSecAnnouce15062023.pdf'
 import pdo from '../pdf/pdoannouce15062023.pdf'
 import pdpa_rmutsv from '../pdf/annouce_pdpa2562.pdf'
+import signed from '../pdf/signed.pdf'
 
 import { Link } from 'react-router-dom'
 
@@ -14,31 +15,32 @@ import { Link } from 'react-router-dom'
 function Home() {
   const [show, setShow] = useState(false);
 
-const handleClose = () => setShow(false);
-const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
-      
+
       {/* <Button variant="primary" onClick={handleShow}>
         Launch demo modal
       </Button> */}
 
       <Modal show={show} onHide={handleClose}
-       size="lg"
-       aria-labelledby="contained-modal-title-vcenter"
-       centered
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
       >
         <Modal.Header closeButton>
           <Modal.Title className='text-center'>Personal Data Protection Act (PDPA)</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <ul className="list-group">
-              <li className='list-group-item'><a class="nav-link" href={appoint} target='_blank'>คำสั่งแต่งตั้งคณะกรรมการคุ้มครองข้อมูลส่วนบุคคล  และ    พระราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
-              <li className='list-group-item'><a class="nav-link" href={pdpa_rmutsv} target='_blank'>ประกาศนโยบายคุ้มครองข้อมูลส่วนบุคคล มทร.ศรีวิชัย พ.ศ. 2566 <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
+          <ul className="list-group">
+            <li className='list-group-item'><a class="nav-link" href={signed} target='_blank'>แนวนโยบายและแนวปฏิบัติในการรักษาความม่ันคงปลอดภัยด้านสารสนเทศ <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
+            <li className='list-group-item'><a class="nav-link" href={appoint} target='_blank'>คำสั่งแต่งตั้งคณะกรรมการคุ้มครองข้อมูลส่วนบุคคล  และ    พระราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
+            <li className='list-group-item'><a class="nav-link" href={pdpa_rmutsv} target='_blank'>ประกาศนโยบายคุ้มครองข้อมูลส่วนบุคคล มทร.ศรีวิชัย พ.ศ. 2566 <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
             <li className='list-group-item'><a class="nav-link" href={pdpa_2562} target='_blank'>หลักเกณฑ์และวิธีการคุ้มครองข้อมูลส่วนบุคคล มทร.ศรีวิชัย พ.ศ.2566 <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
             <li className='list-group-item'><a class="nav-link" href={pdo} target='_blank'>คำสั่งแต่งตั้งเจ้าหน้าที่คุ้มครองข้อมูลส่วนบุคคล DPO<img src="https://upload.wikimedia.org/wikipedia/commons/8/87/PDF_file_icon.svg" width={20} alt="" /></a></li>
-            
-            </ul>
+
+          </ul>
         </Modal.Body>
         {/* <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -53,22 +55,22 @@ const handleShow = () => setShow(true);
 
       <div className="container text-center mt-5">
 
-      <div className="row">
+        <div className="row">
           <div className="col-lg-6 col-md mb-5">
 
             {/* <Link to = '/pdpa'>
               <img src={PDPA_IMG} alt="pdpa image" className='img-fluid rounded shadow' style={{height:'500px'}} />
            </Link> */}
-             <img src={PDPA_IMG} alt="pdpa image" className='img-fluid rounded shadow' style={{height:'500px'}} onClick={handleShow}/>
+            <img src={PDPA_IMG} alt="pdpa image" className='img-fluid rounded shadow' style={{ height: '500px' }} onClick={handleShow} />
           </div>
           <div className="col-lg-6 col-md mb-5">
-            <a  href={cyber15062023} target="_blank">
+            <a href={cyber15062023} target="_blank">
 
-            <img src={Cyber_img} alt="cyber security" className='img-fluid rounded shadow' style={{height:'500px'}}/>
+              <img src={Cyber_img} alt="cyber security" className='img-fluid rounded shadow' style={{ height: '500px' }} />
             </a>
           </div>
-        {/* <Slide /> */}
-      </div>
+          {/* <Slide /> */}
+        </div>
       </div>
       <div className="container d-flex text-center justify-content-center align-items-center">
         <div className="row">
@@ -88,11 +90,11 @@ const handleShow = () => setShow(true);
             <div className="col" style={{ lineHeight: "45px" }}>
               <ul>
                 <li className="list-unstyled">
-                  <i className="fa fa-check-circle text-success" style={{fontSize: "25px"}}></i>{" "}
+                  <i className="fa fa-check-circle text-success" style={{ fontSize: "25px" }}></i>{" "}
                   เพื่อไม่ให้ถูกละเมิดสิทธิความเป็นส่วนตัว
                 </li>
                 <li className="list-unstyled">
-                  <i className="fa fa-check-circle text-success" style={{fontSize: "25px"}}></i>{" "}
+                  <i className="fa fa-check-circle text-success" style={{ fontSize: "25px" }}></i>{" "}
                   เพื่อให้มีมาตรการเยียวยาเจ้าของข้อมูลในกรณีที่ถูกละเมิดข้อมูลส่วนบุคคล
                 </li>
               </ul>
